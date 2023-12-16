b784ad6193b29372c07cd1e3f11a824a576b2418
Edward Townsend <ejpt2@cam.ac.uk>
2023-12-16 18:04:01 +0000

    add: functionality to add selected track element to grid

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gridContainer = document.getElementById('grid-container');
+    let selectedTrack = null; 
   
     // Create the 8x8 grid
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
         const gridItem = document.createElement('div');
-        
+
         if(i === 0 && j === 8){
         } else if(i === 0){
             gridItem.dataset.number = Math.round(Math.random() * 8);
@@ -27,9 +28,17 @@ document.addEventListener('DOMContentLoaded', function() {
                 else {
                     alert('Please enter a number between 0 and 8.');
                 }
-
             });
         }
+        
+        gridItem.addEventListener('click', function() {
+            if(selectedTrack != null){
+                gridItem.id = selectedTrack;
+            }
+            else {
+                alert('Please select a track element.');
+            }
+        });
       }
     }
 
@@ -41,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 element.classList.remove('highlight-border');
             });
             trackElement.classList.add('highlight-border');
+            selectedTrack = trackElement.id;
         });
     });
-  });
-  
+});
