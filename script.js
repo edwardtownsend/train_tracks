e37afc78356aa5e1a2d8c3489a6f27fe634927e2
Edward Townsend <ejpt2@cam.ac.uk>
2023-12-16 15:36:15 +0000

    8x8 grid with editable numbers on the outisde finished

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const gridContainer = document.getElementById('grid-container');
   
     // Create the 8x8 grid
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
         const gridItem = document.createElement('div');
         
-        if(i === 0){
+        if(i === 0 && j === 8){
+        } else if(i === 0){
             gridItem.dataset.number = Math.round(Math.random() * 8);
-            gridItem.classList.add('top-grid-item');
-        } else if(j === 7){
+            gridItem.classList.add('top-edge');
+        } else if(j === 8){
             gridItem.dataset.number = Math.round(Math.random() * 8);
-            gridItem.classList.add('bot-grid-item');
+            gridItem.classList.add('right-edge');
         } else{
             gridItem.classList.add('grid-item');
         }
         gridContainer.appendChild(gridItem);
 
-        if(i === 0 || j === 7){
+        if ((i === 0 || j === 8) && !(i === 0 && j === 8)) {
             gridItem.addEventListener('click', function() {
                 const newNumber = prompt('Enter a number:');
                 if (newNumber !== null) {
